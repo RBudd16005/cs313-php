d4b4236e9b655d472d3a298a6ebad150590282af
Ryan <bud16005@byui.edu>
2019-10-26 15:42:49 -0600

    added new pages

---
+++ web/home.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+        <meta charset="UTF-8">
+        <title>Sound Biscuit</title>
+        <link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+<div>
+            <h1>
+                Sound Biscuit
+            </h1>
+            <?php 
+                date_default_timezone_set('MST');
+                echo "<p>Current Time and Date: </p>";
+                echo date('h:i A e; l F jS, Y');
+            ?>
+        </div>
+</body>
+</html>
